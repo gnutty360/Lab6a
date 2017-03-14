3d2439b0523e71c9e9d702b4e5872385f16c58c8
Talyia Fordham <Talyia Fordham>
2017-03-13 20:32:19 -0500

    Working default constructor

--- lab6.cpp
+++ lab6.cpp
@@ -10,7 +10,7 @@
 #include <string>
 using namespace std;
 
-#define DEFAULT_DATA_FILE "defaultData"
+#define DEFAULT_DATA_FILE "defaultData.txt"
 
 class Signal{
 	private:
@@ -22,36 +22,142 @@ class Signal{
 		Signal();
 		Signal(int);
 		Signal(char*);
+		~Signal();
 
 };
 
-Signal::Signal(){
-
+Signal::~Signal(){
 	
+	delete [] data;
+	if(data == NULL){
+		cout<<"Data deletion success"<<endl;
+	}
+	
+}
 
+Signal::Signal(){
+	
+	fstream dataFile;
+	int buffer, i;
+	
+	dataFile.open(DEFAULT_DATA_FILE);
+	
+	if(!dataFile.is_open()){
+		cout<<"\nCould not open "<<DEFAULT_DATA_FILE<<" setting default values!"<<endl;
+		length = 0;
+		max_value = 0;
+		average = 0;
+		data = NULL;
+		return;
+	}
+	
+	dataFile >> length; //Read number of integers in the datafile
+	
+	data = new double[length];
+	if(data == NULL){
+		cout<<"Could not allocate memory for data in default constructor!"
+			<<"setting default values and exiting!"<<endl;
+		length = 0;
+		max_value = 0;
+		average = 0;
+		data = NULL;
+		return;
+	}
+	
+	dataFile >> max_value; //Read the max value of the data
+	
+	for(i = 0; length; i++){
+		dataFile >> data[i];
+	}
+	
+	
+	dataFile.close();
+	return;
+}
+
+Signal::Signal(int L){
+	cout<<"FileNumber Constructor!"<<endl;
+	length = 0;
+	max_value = 0;
+	average = 0;
+	data = NULL;
+}
+
+Signal::Signal(char* fileName){
+	cout<<"FileName Constructor!"<<endl;
+	length = 0;
+	max_value = 0;
+	average = 0;
+	data = NULL;
+	
 }
 
 
 void setFileName(string*, char**);
 
+
 int main(int argc, char** argv){
 	double* data;
 	fstream dataFile;
+	
+	int userInput1;
+	char userInput2;
+	char fileName[25];
 
-	string fileName;
 	
-	if(checkInputs(argc, argv)){
-		cout<<endl<<"Invlaid command line arguments. Terminating!"<<endl;
+	cout<<"\n****Lab6****"<<endl;
+	while(1){
+		
+		cout<<"\nWould you like to use the default data file?(Y N)"<<endl;
+		cin >> userInput2;
+		while( userInput2 != 'y' &&
+			userInput2 != 'Y' &&
+			userInput2 != 'n' &&
+			userInput2 != 'N')
+			{
+				cout<<"\nInvalid input!"<<endl;
+				cout<<"\nWould you like to use the default data file?(Y N)"<<endl;
+				cin >> userInput2;
+				
+			}
+			
+		if(userInput2 == 'Y' || userInput2 == 'y'){
+			cout<<"\nUsing the default constructor"<<endl;
+			Signal dataSignal;
+		}else{
+			cout<<"\nNot using default constructor"<<endl;
+			cout<<"\nEnter 'f' to use the file name or 'n' to use the file number:"<<endl;
+			cin >> userInput2;
+			
+			while( userInput2 != 'n' &&
+			userInput2 != 'N' &&
+			userInput2 != 'f' &&
+			userInput2 != 'F')
+			{
+				cout<<"\nInvalid input!"<<endl;
+				cout<<"\nEnter 'f' to use the file name or 'n' to use the file number:"<<endl;
+				cin >> userInput2;
+				
+			}
+			
+			if(userInput2 == 'f'|| userInput2 == 'F'){
+				cout<<"\nPlease enter the file name to be opened:"<<endl;
+				cin >> fileName;
+				Signal dataSignal(fileName);
+			}else{
+				cout<<"\nPlease enter the file number to be opened:"<<endl;
+				cin >> userInput1;
+				Signal dataSignal(userInput1);
+			}
+		}
+		
+		
+		
+		
+		
 	}
 	
-	
-	
-	dataFile.open(fileName);
-	if(!datafile.is_open()){
-		count<<endl<<"Unable to open file "<<fileName<<". Quitting."<<endl;
-	}
-	
-	 
+
 	
 return(0);
 }
@@ -66,23 +172,6 @@ void setFileName(string* strp, char* value){
 
 
 
-/*
-int checkInputs(int argc, char** argv){
 
-	int i;
-	string str1 = argv[1];
-	if(str1.compare("-n") == 0 || str1.compare("-N") == 0){
-	
-		str1 = argv[2];
-		i = stoi(str1);
-		cout<<endl<<i<<endl;	
-	
-	}else if(str1.compare("-f")){
 
-	}else{
 
-	return(1);
-	
-	}
-}
-*/
